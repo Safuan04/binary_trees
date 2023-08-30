3b2c9de083cfc7a76a9b559bbaa1762feca2d10b
Safuan04 <elmai.safuan@gmail.com>
2023-08-30 06:30:49 -0700

    Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,6 +22,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new_node->parent = parent;
 	new_node->n = value;
+	new_node->left = parent->left;
 	new_node->right = NULL;
 
 	if (parent->left != NULL)
